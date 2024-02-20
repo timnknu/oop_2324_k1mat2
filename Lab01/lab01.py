aa5f1fef7de2bf418fdb2e4aa6b155300b64c750
timn <i-wont-tell@you.com>
2024-02-20 14:27:59 +0200

    first obj added

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -1,12 +1,20 @@
-e = {'a': 1.0, 'b': -2.0}
-e['c'] = 0.5
-def solve_eq(data):
-    D = data['b'] ** 2 - 4 * data['a'] * data['c']
-    if D > 0:
-        x1 = (-data['b'] - D**0.5) / (2 * data['a'])
-        x2 = (-data['b'] + D**0.5) / (2 * data['a'])
-        print(x1, x2)
-    else:
-        print('No solution')
 
-solve_eq(e)
+
+class QuadraticEquation:
+    def show(self):
+        print(self.a, self.b)
+
+    def solve_eq(data):
+        D = data.b ** 2 - 4 * data.a * data.c
+        if D > 0:
+            x1 = (-data.b - D**0.5) / (2 * data.a)
+            x2 = (-data.b + D**0.5) / (2 * data.a)
+            print(x1, x2)
+        else:
+            print('No solution')
+
+eq = QuadraticEquation()
+eq.a = 1.0
+eq.b =-2.0
+eq.c = 0.5
+eq.solve_eq()
