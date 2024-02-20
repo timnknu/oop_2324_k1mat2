2946c17fc4311e27dc6dce57ee1f155c76c713c1
timn <i-wont-tell@you.com>
2024-02-20 15:30:51 +0200

    copy constructor added

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -1,8 +1,13 @@
 class QuadraticEquation:
-    def __init__(self, coef_a, coef_b, coef_c):
-        self.a = coef_a
-        self.b = coef_b
-        self.c = coef_c
+    def __init__(self, coef_a, coef_b = None, coef_c = None):
+        if isinstance(coef_a, QuadraticEquation):
+            self.a = coef_a.a
+            self.b = coef_a.b
+            self.c = coef_a.c
+        else:
+            self.a = coef_a
+            self.b = coef_b
+            self.c = coef_c
 
     def show(self):
         print(f'({self.a}) * x**2 + ({self.b}) * x + ({self.c}) = 0')
@@ -23,9 +28,10 @@ class QuadraticEquation:
 eq = QuadraticEquation(1.0, -2.0, 1.0) # 1 sol
 second_eq = QuadraticEquation(1.0, -3.0, 1.5) # 2 sol
 third_eq = QuadraticEquation(1.0, -3.0, 10.5)  # 0 sol
+cloned_equation = QuadraticEquation(eq)
 
 
-for obj in [eq, second_eq, third_eq]:
+for obj in [eq, second_eq, third_eq, cloned_equation]:
     s = obj.solve_eq()
     if len(s) == 1:
         obj.show()
