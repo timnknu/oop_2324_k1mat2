2a6642eb0f6fbe5d6d389ee86ad9459973788629
timn <i-wont-tell@you.com>
2024-02-27 15:38:36 +0200

    getter for powers added

--- 02_Feb27/L01_146.py
+++ 02_Feb27/L01_146.py
@@ -1,7 +1,7 @@
 import copy
 class Polynom:
     def __init__(self):
-        self._data = {}
+        self.__data = {}
     def _process_line(self, line):
         rec = line.split()
         if len(rec) > 0:
@@ -17,17 +17,17 @@ class Polynom:
             except:
                 print("Wrong coef", rec[1])
                 return
-            assert pwr not in self._data
-            self._data[pwr] = coef
+            assert pwr not in self.__data
+            self.__data[pwr] = coef
 
     def read_form_file(self, file_name):
-        self._data = {}
+        self.__data = {}
         with open(file_name) as f:
             for line in f:
                 self._process_line( line.strip() )
     #
     def read_from_keyboard(self):
-        self._data = {}
+        self.__data = {}
         print("Input powers & coefs, or empty line to stop")
         while True:
             s = input()
@@ -37,32 +37,35 @@ class Polynom:
 
     #
     def show(self):
-        print(self._data)
+        print(self.__data)
 
     def evaluate_at_x(self, x):
         result = 0
-        for pwr, cf in self._data.items():
+        for pwr, cf in self.__data.items():
             result += x**pwr * cf
         return result
 
     # 'getter' для коефіцієнтів
     def get_coef(self, pwr):
         #return self._data.get(pwr, 0.0)
-        if pwr in self._data:
-            return self._data[pwr]
+        if pwr in self.__data:
+            return self.__data[pwr]
         else:
             return 0.0
 
     # setter for coefs
     def set_coefs(self, coefs_dict):
         assert isinstance(coefs_dict, dict)
-        self._data = coefs_dict.copy()
+        self.__data = coefs_dict.copy()
+
+    def get_powers(self):
+        return self.__data.keys()
 
     @staticmethod
     def add(p1, p2):
         r = {}
-        powers1 = p1._data.keys()
-        powers2 = p2._data.keys()
+        powers1 = p1.get_powers()
+        powers2 = p2.get_powers()
         for pw in set(powers1) | set(powers2):
             cf = p1.get_coef(pw) + p2.get_coef(pw)
             r[pw] = cf
