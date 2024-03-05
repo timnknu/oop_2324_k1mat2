1e34457ae2f4a9cff367cce7a18e49d607babae3
timn <i-wont-tell@you.com>
2024-03-05 15:16:02 +0200

    QuadraticEquation moved to a separate file

---
+++ 03_Lab03/quadr_eq.py
@@ -0,0 +1,21 @@
+from linear_eq import Equation
+
+class QuadraticEquation(Equation):
+    def __init__(self, a, b, c):
+        self._a = a
+        super().__init__(b, c)
+    #
+    def solve(self):
+        if self._a == 0:
+            return super().solve()
+        else:
+            D = self._b ** 2 - 4 * self._a * self._c
+            if D > 0:
+                x1 = (-self._b - D**0.5) / (2 * self._a)
+                x2 = (-self._b + D**0.5) / (2 * self._a)
+                return (x1, x2)
+            elif abs(D) < 1e-10:
+                x = (-self._b) / (2 * self._a)
+                return (x, )
+            else:
+                return Equation.NO_SOLS
