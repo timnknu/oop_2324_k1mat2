950d72f68aed056343a5b3e1c7de1078100c42d4
timn <i-wont-tell@you.com>
2024-03-05 15:32:19 +0200

    BiQuadraticEquation implemented

--- 03_Lab03/main.py
+++ 03_Lab03/main.py
@@ -9,11 +9,25 @@ class BiQuadraticEquation(QuadraticEquation):
         if r == Equation.INF_SOLS:
             return Equation.INF_SOLS
         #
+        roots = []
         for t in r:
-            print(t)
+            if t>0:
+                roots.append( t**0.5 )
+                roots.append(- t ** 0.5)
+            if t==0:
+                roots.append(0)
+            #
+        return tuple(roots)
+#
 
+#e = BiQuadraticEquation(1, 2, -3) # (x**2 - 1)*(x**2 + 3) = 0
+e = BiQuadraticEquation(1, 3, 0) # x**2*(x**2 + 3) = 0
+#e = BiQuadraticEquation(1, -4, 3) # (x**2 - 1)*(x**2 - 3) = 0
+#e = BiQuadraticEquation(1, -3, 0) # x**2*(x**2 - 3) = 0
 
-e = BiQuadraticEquation(1, 2, -3) # (x**2 - 1)*(x**2 + 3) = 0
+#e = BiQuadraticEquation(1, 4, 3) # (x**2 + 1)*(x**2 + 3) = 0
+#e = BiQuadraticEquation(0, 0, 0)
+#e = BiQuadraticEquation(0, 1, -1) # x**2 - 1 = 0
 r = e.solve()
-#print(r, len(r))
+print(r, len(r))
 
