b5f44b796bd20f6bb8a15b17e84c54c2c7b493e6
timn <i-wont-tell@you.com>
2024-03-05 14:54:29 +0200

    INF_SOLS and NO_SOLS made static

--- 03_Lab03/main.py
+++ 03_Lab03/main.py
@@ -1,7 +1,8 @@
-INF_SOLS = (0, 0, 0, 0, 0)
-NO_SOLS = tuple()
 
 class Equation:
+    INF_SOLS = (0, 0, 0, 0, 0)
+    NO_SOLS = tuple()
+
     def __init__(self, b, c):
         self._b = b
         self._c = c
@@ -9,18 +10,20 @@ class Equation:
         # b*x + c = 0 => x = -c/b
         if self._b == 0:
             if self._c == 0:
-                return INF_SOLS
+                return Equation.INF_SOLS
             else:
-                return NO_SOLS
+                return Equation.NO_SOLS
         else:
             x = -self._c / self._b
             return (x,)
     #
 #
-#e = Equation(-2, -1)
+e = Equation(-2, -1)
 #e = Equation(-2, 0)
 #e = Equation(0, -1)
-e = Equation(0, 0)
+#e = Equation(0, 0)
 r = e.solve()
 print(r, len(r))
 
+#print(Equation.INF_SOLS)
+#print(e.INF_SOLS)
