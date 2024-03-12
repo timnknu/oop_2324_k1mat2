b87778275d1c35a647fe4273bb3ab736545ad95d
timn <i-wont-tell@you.com>
2024-03-12 14:41:35 +0200

    turtle-ex

--- 04_Lab04/turtle_demo.py
+++ 04_Lab04/turtle_demo.py
@@ -1,8 +1,15 @@
 import turtle
 
 turtle.home()
+
 turtle.down()
 turtle.forward(50)
+turtle.up()
+
+turtle.forward(20)
+
+turtle.down()
+turtle.forward(80)
 
 #turtle.mainloop()
 turtle.exitonclick()
