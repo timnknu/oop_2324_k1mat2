a2fcd0a9de6305580e7eca2e1b9ae4e33eaa84da
timn <i-wont-tell@you.com>
2024-03-12 14:50:05 +0200

    turtle-ex4

--- 04_Lab04/turtle_demo.py
+++ 04_Lab04/turtle_demo.py
@@ -1,10 +1,11 @@
 import turtle
 
-for a in range(0, 270, 10):
-    turtle.home()
+turtle.goto(0, -100)
+
+for a in range(0, 270, 20):
     turtle.down()
     turtle.setheading(a)
-    turtle.forward(120)
+    turtle.forward(50)
     turtle.up()
 
 #turtle.mainloop()
