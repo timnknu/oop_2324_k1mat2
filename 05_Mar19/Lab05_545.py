205883f4a268e9ccab61f9cdf4dd491a651f991c
timn <i-wont-tell@you.com>
2024-03-19 14:41:34 +0200

    __str__ added

--- 05_Mar19/Lab05_545.py
+++ 05_Mar19/Lab05_545.py
@@ -1,15 +1,17 @@
+import copy
+
 class Vector:
     def __init__(self, n):
         if isinstance(n, Vector):
-            self._data = n._data
+            self._data = copy.deepcopy(n._data)
         else:
             assert type(n) is int
             assert n>0
             self._data = [0.0]*n
+    def __str__(self):
+        #s = '{'
+        s = str(self._data)
+        return s
 
 a = Vector(3)
-
-x = [1,2,3]
-y = x
-y[0] = -100
-print(x)
+print(a)
