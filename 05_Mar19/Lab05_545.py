8a313c86e3b4fde672d6f012f6642ef341b625a9
timn <i-wont-tell@you.com>
2024-03-19 14:55:14 +0200

    __add__ v2

--- 05_Mar19/Lab05_545.py
+++ 05_Mar19/Lab05_545.py
@@ -13,13 +13,24 @@ class Vector:
         s = str(self._data)
         return s
     def __add__(self, other):
+        res = Vector(len(self._data))
         if isinstance(other, Vector):
             assert len(self._data) == len(other._data)
-            res = Vector(len(self._data))
             for i in range(len(self._data)):
                 res._data[i] = self._data[i] + other._data[i]
+            # альтернативна реалізація:
+            #res = Vector(self)
+            #for i in range(len(self._data)):
+            #   res._data[i] += other._data[i]
+            return res
+        else:
+            for i in range(len(self._data)):
+                res._data[i] = self._data[i] + other
             return res
 
+
+
 a = Vector(3)
-b = a + 1
+a._data[0] = 1.2
+b = a + 9
 print(b)
