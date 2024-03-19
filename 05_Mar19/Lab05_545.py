1420b3ad1f7ed1adb4bd0ea318a4bafb8fcf4dd3
timn <i-wont-tell@you.com>
2024-03-19 14:36:05 +0200

    __init__ v1

--- 05_Mar19/Lab05_545.py
+++ 05_Mar19/Lab05_545.py
@@ -1,3 +1,15 @@
 class Vector:
     def __init__(self, n):
-        self._data = [0.0]*n
+        if isinstance(n, Vector):
+            self._data = n._data
+        else:
+            assert type(n) is int
+            assert n>0
+            self._data = [0.0]*n
+
+a = Vector(3)
+
+x = [1,2,3]
+y = x
+y[0] = -100
+print(x)
