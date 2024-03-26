81f9ea0db693f4b0989fb186f45c15e20eb46ccf
timn <i-wont-tell@you.com>
2024-03-26 14:58:40 +0200

    __call___ demo

--- 05_Mar19/Lab05_545_matrix.py
+++ 05_Mar19/Lab05_545_matrix.py
@@ -40,11 +40,7 @@ class Vector:
         #return self.__add__(other)
         return self + other
     def __mul__(self, other):
-        #res = Vector(self)
-        #res = copy.deepcopy(self)
-        #C = type(self)
-        C = self.__class__
-        res = C(self)
+        res = copy.deepcopy(self)
         assert (type(other) is int or type(other) is float)
         for i in range(len(self)):
             res[i] *= other
@@ -56,16 +52,14 @@ class Matrix(Vector):
     SEP = '\n'
     def __init__(self, n):
         super().__init__(n)
+    def __call__(self, a):
+        print(a)
+        return -100
 
 row1 = Vector([7,8,9])
 row2 = Vector([1,2,3])
-print(row1 + row2)
 row3 = Vector([-2,1,0])
 m = Matrix([row1, row2, row3])
-#p = m + 10
-#p = 10 + m
-#p = m * 2.0
-print(m)
-p = m * 2.0
-print(p)
 
+result = m('he')
+print(result)
