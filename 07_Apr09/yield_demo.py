1eb955e074592aa63a15e9d2b0b0ad9c8f2bcd33
timn <i-wont-tell@you.com>
2024-04-09 14:48:26 +0300

    three yields

--- 07_Apr09/yield_demo.py
+++ 07_Apr09/yield_demo.py
@@ -1,11 +1,20 @@
-
 def f():
-    #....1
-    #....2
+    tmpl = 3.14
+    print('A', tmpl)
     yield -15
-    #....3
+    print('B', tmpl)
+    yield -54
+    print('С', tmpl)
+    yield 100500
+    print('D', tmpl)
 
 v = f()
 print(v)
-for nm in dir(v):
-    print(nm)
+for elem in v:
+    print(elem)
+
+# b = iter(v)
+# print(next(b))
+# print(next(b))
+# print(next(b))
+# print(next(b))
