1d010e9effb951ff6715455941ee55c491fbb2a3
timn <i-wont-tell@you.com>
2024-04-09 15:18:24 +0300

    sum(generator)

---
+++ 07_Apr09/p03.py
@@ -0,0 +1,17 @@
+def etgen(x, eps):
+    a = 1.0
+    yield a
+    n = 1
+    while a > eps:
+        a = a * x/n
+        n += 1
+        yield a
+#
+x = 0.12
+v = etgen(x, 1e-5)
+print(sum(v))
+# s = 0
+# for el in v:
+#     s += el
+#     print(el)
+# print('sum is', s)
