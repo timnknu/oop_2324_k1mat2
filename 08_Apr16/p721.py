a5fe2bd275c6f0b96f6f766800756a953495365e
timn <i-wont-tell@you.com>
2024-04-16 15:23:32 +0300

    catching general exception

--- 08_Apr16/p721.py
+++ 08_Apr16/p721.py
@@ -1,18 +1,21 @@
 import traceback
 
-class ProtectedDictIntError(KeyError):
-    ERR_NONINT_KEY = 1
-    ERR_EXISTING_KEY = 2
-
-    def __init__(self, err_type, a):
+class ProtectedDictIntGeneralError(KeyError):
+    def __init__(self, a):
         super().__init__(a)
         self._useful_info = a
-        self._err_typ = err_type
     def __str__(self):
-        if self._err_typ == self.ERR_EXISTING_KEY:
-            s = f"ключ {self._useful_info} уже існує"
-        elif self._err_typ == self.ERR_NONINT_KEY:
-            s = f"ключ {self._useful_info} не був цілим значенням"
+        s = f"ключ {self._useful_info} є помилковим"
+        return s
+
+class NonIntegerKeyError(ProtectedDictIntGeneralError):
+    def __str__(self):
+        s = f"ключ {self._useful_info} із NonIntegerKeyError"
+        return s
+
+class KeyAlreadyExistsError(ProtectedDictIntGeneralError):
+    def __str__(self):
+        s = f"ключ {self._useful_info} із KeyAlreadyExistsError"
         return s
 
 class ProtectedDictInt:
@@ -29,29 +32,18 @@ class ProtectedDictInt:
             if key not in self._data:
                 self._data[key] = value
             else:
-                raise ProtectedDictIntError(ProtectedDictIntError.ERR_EXISTING_KEY, key)
+                raise KeyAlreadyExistsError(key)
         else:
-            obj = ProtectedDictIntError(ProtectedDictIntError.ERR_NONINT_KEY, key)
-            raise obj
+            raise NonIntegerKeyError(key)
 
 d = ProtectedDictInt()
-
-#d[2.0] = -100500
 
 try:
     d[1] = 12.0
     d[2.0] = -100500 #/0
     d[1] = -8
     print('---')
-except ProtectedDictIntError as e:
+except ProtectedDictIntGeneralError as e:
     print('виникла помилка', e)
-    #s = traceback.format_exc()
-    #print('---')
-    #print(e._useful_info)
-    #print(e.__traceback__.tb_lineno)
-    #print('---')
-    #print('старе повідомлення про помилку -', s)
-except ZeroDivisionError:
-    print('Ділити на нуль не можна!')
 
 print(d)
