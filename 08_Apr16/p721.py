41b8e141e25d3498b18f19745294bb0a53eaa41e
timn <i-wont-tell@you.com>
2024-04-16 14:47:32 +0300

    exception class added, try...except added

--- 08_Apr16/p721.py
+++ 08_Apr16/p721.py
@@ -1,3 +1,11 @@
+class ProtectedDictIntError(KeyError):
+    def __init__(self, a):
+        super().__init__(a)
+        self._useful_info = a
+    def __str__(self):
+        s = f"ключ {self._useful_info} не був цілим значенням"
+        return s
+
 class ProtectedDictInt:
     def __init__(self):
         self._data = {}
@@ -7,14 +15,23 @@ class ProtectedDictInt:
         pass
     def __setitem__(self, key, value):
         if isinstance(key, int):
+        # if type(key) is int: # альтернативна перевірка
             # в key -- ціле число => це допутисмий ключ
             self._data[key] = value
         else:
-            print('помилка')
+            obj = ProtectedDictIntError(key)
+            raise obj
 
 d = ProtectedDictInt()
 
-d[1] = 12.0
-d[2.0] = -100500
-print('---')
+try:
+    d[1] = 12.0
+    d[2.0] = -100500
+    print('---')
+except ProtectedDictIntError as e:
+    print('виникла помилка', e)
+    pass
+except ZeroDivisionError:
+    print('Ділити на нуль не можна!')
+
 print(d)
