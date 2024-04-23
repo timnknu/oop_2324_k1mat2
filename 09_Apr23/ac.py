95af6d5c807dbaa9ae951036ca7861e78d77c25a
timn <i-wont-tell@you.com>
2024-04-23 14:36:58 +0300

    print_minmax() implemented

--- 09_Apr23/ac.py
+++ 09_Apr23/ac.py
@@ -6,11 +6,18 @@ class SequenceAnalyzer(metaclass=ABCMeta):
         pass
     #
     def print_minmax(self):
-        for el in self.terms_gen():
-            k, ak = el
-            print(k, ak)
-            if k> 10:
+        max_val = None # ініціалізація одним способом
+        for j, (k, ak) in enumerate(self.terms_gen()):
+            if max_val is None or ak > max_val:
+                max_val = ak
+            if j==0:
+                min_val = ak # ініціалізація іншим способом
+            if ak < min_val:
+                min_val = ak
+            if j > 10:
                 break
+        print("min =", min_val)
+        print("max =", max_val)
 
 class SinSeq(SequenceAnalyzer):
     def terms_gen(self):
