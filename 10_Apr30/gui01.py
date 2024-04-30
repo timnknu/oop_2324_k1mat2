0889e1792701e181e52173e7a11b04cc91c0e31a
timn <i-wont-tell@you.com>
2024-04-30 15:03:57 +0300

    cleaned version

--- 10_Apr30/gui01.py
+++ 10_Apr30/gui01.py
@@ -14,31 +14,27 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GButton_353=tk.Button(root)
-        GButton_353["text"] = "Старт"
+        GButton_353 = tk.Button(root, text = "Старт",
+                                command = self.GButton_353_command
+                                )
         GButton_353.place(x=60,y=140,width=111,height=54)
-        GButton_353["command"] = self.GButton_353_command
 
-        GLabel_119=tk.Label(root)
-        GLabel_119["text"] = "Кількість секунд"
+        GLabel_119=tk.Label(root, text = "Кількість секунд")
         GLabel_119.place(x=30,y=50,width=70,height=25)
 
-        GButton_925=tk.Button(root)
-        GButton_925["text"] = "Стоп"
+        GButton_925=tk.Button(root, text = "Стоп",
+                              command = self.GButton_925_command)
         GButton_925.place(x=280,y=160,width=70,height=25)
-        GButton_925["command"] = self.GButton_925_command
 
         GLineEdit_759=tk.Entry(root)
-        GLineEdit_759["borderwidth"] = "1px"
-        GLineEdit_759["text"] = "Entry"
         GLineEdit_759.place(x=170,y=50,width=70,height=25)
 
     def GButton_353_command(self):
-        print("command")
+        print("AAAAA")
 
 
     def GButton_925_command(self):
-        print("command")
+        print("bbbb")
 
 if __name__ == "__main__":
     root = tk.Tk()
